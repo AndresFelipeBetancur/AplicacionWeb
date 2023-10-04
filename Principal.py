42a255473e1d0ba138bdf421725a2b3734e41908
AndresFelipeBetancur <abetancurquintero066@gmail.com>
2023-10-04 16:13:46 -0500

    carga

--- Principal.py
+++ Principal.py
@@ -1,11 +1,15 @@
-
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template,send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def raiz():
     return render_template("/raiz.html")
+
+@app.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory('uploads', filename)
 
 @app.route("/iniciaSesion")
 def Sesion():
